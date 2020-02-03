84333b39d7421e565046d1f21bbe6df5c520f0fa
unknown <thiago.kaiser@hotmail.com>
2020-02-03 11:40:29 -0300

    first

---
+++ CqrsEventSourcing/Program.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace CqrsEventSourcing
+{
+    public class Person
+    {
+        private int age;
+
+    }
+    public class EventBroker
+    {
+        public IList<Event> AllEvents = new List<Event>();
+
+    }
+    public class Event
+    {
+
+    }
+
+
+
+
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+            var p = new Person();
+            
+        }
+    }
+}
